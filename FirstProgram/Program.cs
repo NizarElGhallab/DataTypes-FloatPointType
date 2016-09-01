ba5b55cf03a36fa5793f0630683109ecfd53b7d6
Nizar <Nizar@NIIZAR>
2016-09-01 18:50:29 +0100

    Add a comment

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -10,6 +10,8 @@ namespace FirstProgram
     {
         static void Main(string[] args)
         {
+            //This my first program
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
 
